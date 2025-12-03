62a197d0d995d9a96c13ad482915cb57a2aeddd7
Katherine <kaburbank@gmail.com>
2025-12-03 11:15:51 -0700

    create blog site using react components and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header.jsx";
+import About from "./About.jsx";
+import ArticleList from "./ArticleList.jsx";
+import blogData from "../data/blog.js";
 
 function App() {
+  const { name, image, about, posts } = blogData;
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={name} />
+      <About image={image} about={about} />
+      <ArticleList posts={posts} />
     </div>
   );
 }
